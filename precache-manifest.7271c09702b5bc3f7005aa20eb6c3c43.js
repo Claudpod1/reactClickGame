e87008d2ec2f62471e6868b961c8e0ad78bc2eaa
Claud Pod <claudiapollinger.cp@gmail.com>
2019-09-08 21:43:31 -0600

    Updates

---
+++ precache-manifest.7271c09702b5bc3f7005aa20eb6c3c43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cdf8f3cc9eecd8b318d819a9b389d2",
+    "url": "/reactClickGame/index.html"
+  },
+  {
+    "revision": "50cccf59900be66287e8",
+    "url": "/reactClickGame/static/css/main.965a2c45.chunk.css"
+  },
+  {
+    "revision": "be72b1c9ac13d3fd39da",
+    "url": "/reactClickGame/static/js/2.c6be5ced.chunk.js"
+  },
+  {
+    "revision": "50cccf59900be66287e8",
+    "url": "/reactClickGame/static/js/main.07f49dce.chunk.js"
+  },
+  {
+    "revision": "cd9c49a5a0589e0a8a5a",
+    "url": "/reactClickGame/static/js/runtime~main.49359f35.js"
+  },
+  {
+    "revision": "cfb9999fcfa7f5c2c6b3d464b1769052",
+    "url": "/reactClickGame/static/media/flag.cfb9999f.jpg"
+  }
+]);
